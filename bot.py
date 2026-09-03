1ca3b2b6f14dacf07875347c242e3eba0a3934a6
tgn6yb7tn8-dotcom <tgn6yb7tn8@privaterelay.appleid.com>
2026-09-03 11:55:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ from analyzer import analyser_plusieurs_images
 TOKEN = os.getenv("DISCORD_TOKEN", "TON_TOKEN_ICI")
 
 VERIFICATION_CHANNEL_ID = 1544613786952925326
-RESULT_CHANNEL_ID = 1383758654901194875
+RESULT_CHANNEL_ID = 1544613786952925326
 
 # Les commandes de gestion seront utilisables uniquement
 # dans le salon des résultats.
